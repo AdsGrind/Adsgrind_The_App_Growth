25cc70ed0411ca44d72ef646b6dedc0ba7ed2bd0
Adsgrind <partner@adsgrind.com>
2026-05-03 04:50:59 +0530

    done2

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  experimental: {
-    allowedDevOrigins: ['172.21.181.125'],
-  },
 };
 
 export default nextConfig;
